89dc7b1d6b7fef8f2a3a818cb3faf286289f5476
Hon Boey <hon@dialogicstudios.com.au>
2021-10-02 18:42:30 +1000

    Initalised project. Created data. WIP array of wrestler's weights

---
+++ data/wweChampionsSample.js
@@ -0,0 +1,13 @@
+module.exports.wweChampionsSample = [
+    {
+        name: "Buddy Rogers",
+        height: 165,
+        weight: 235
+    },
+    {
+        name: "John Cena",
+        height: 201,
+        weight: 199
+    }
+]
+
---
+++ test.js
@@ -0,0 +1,9 @@
+wweChampions = require("./data/wweChampions.js").wweChampions
+wrestlerHeightAndWeight = require("./data/wrestlerHeightAndWeight.js").wrestlerHeightAndWeight
+
+// Array Array -> Array
+// Find the values of an array in a corresponding array
+/*
+check-expect [{name: "Buddy Rogers"}, ]
+*/
+
